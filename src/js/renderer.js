121648a95b776ae60b6565a92e415df0ac6c71a5
Rukshan Lakshitha <rukshandangalla@gmail.com>
2017-02-10 14:09:09 +0530

    adding editable column

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,3 +1,27 @@
+var documentData;
+const gridColumns = [
+    {field: 'id', title: 'Id'},
+    {field: 'filename', title: 'File Name', editable: {type: 'text'}},
+    {field: 'numberofpages', title: 'Number Of Pages'},
+    {field: 'client', title: 'Client'},
+    {field: 'scannerseries', title: 'Scanner series'},
+    {field: 'documenttype', title: 'Document type'},
+    {field: 'scanningdate', title: 'Scanning Date'},];
+
 $(document).ready(_ => {
-    
+    $.getJSON("http://localhost:3000/Documents", PopulateMainGrid);
 });
+
+/**
+ * populate main data grid
+ * 
+ * @param {any} data
+ */
+function PopulateMainGrid(data){
+    documentData = data;
+    
+    $("#mainGrid").bootstrapTable({
+        columns: gridColumns,
+        data: documentData
+    });
+}

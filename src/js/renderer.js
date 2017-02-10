a7c84139ed5fab2bbfd16cef23ddae3b5f1725b0
Rukshan Lakshitha <rukshandangalla@gmail.com>
2017-02-10 17:06:34 +0530

    date picker and new layout

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,12 +1,12 @@
 var documentData;
 const gridColumns = [
-    {field: 'id', title: 'Id'},
-    {field: 'filename', title: 'File Name', editable: {type: 'text'}},
-    {field: 'numberofpages', title: 'Number Of Pages'},
-    {field: 'client', title: 'Client'},
-    {field: 'scannerseries', title: 'Scanner series'},
-    {field: 'documenttype', title: 'Document type'},
-    {field: 'scanningdate', title: 'Scanning Date'},];
+    {field: 'id', title: 'Id', class: 'main-header'},
+    {field: 'filename', title: 'File Name', class: 'main-header', editable: {type: 'text', mode: 'inline', showbuttons: false}},
+    {field: 'numberofpages', title: 'Number Of Pages', class: 'main-header'},
+    {field: 'client', title: 'Client', class: 'main-header'},
+    {field: 'scannerseries', title: 'Scanner series', class: 'main-header'},
+    {field: 'documenttype', title: 'Document type', class: 'main-header'},
+    {field: 'scanningdate', title: 'Scanning Date', class: 'main-header', editable:{type: 'date',  viewformat: 'yyyy-mm-dd'}}]; //editable: {type: 'date'}format: 'dd-mm-yyyy', viewformat: 'dd-mm-yyyy'
 
 $(document).ready(_ => {
     $.getJSON("http://localhost:3000/Documents", PopulateMainGrid);
